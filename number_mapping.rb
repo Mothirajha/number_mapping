faa8f1051157ba926f9f65f131b0136bbeb02fc6
Mothirajha <mothi@Mothirajhas-MacBook-Pro.local>
2019-01-10 02:03:28 +0530

    [Update] mix up letter arrays

--- number_mapping.rb
+++ number_mapping.rb
@@ -1,6 +1,7 @@
 class NumberMapping
 
-  attr_reader :dictionary
+  attr_reader :dictionary, :numbers
+  attr_accessor :mapped_letters
 
   LETTERS = {
     2 => ["a", "b", "c"],
@@ -13,9 +14,13 @@ class NumberMapping
     9 => ["w", "x", "y", "z"],
   }
 
-  def initialize
+  def initialize(numbers)
     @dictionary = []
+    @numbers = numbers
     load_dictionary
+    @mapped_letters = map_digits_to_letters
+    @total_length = @mapped_letters.length
+    @min_count = 3
   end
 
   def load_dictionary
@@ -23,6 +28,22 @@ class NumberMapping
     File.open( file_path, 'r' ).each do |line|
       @dictionary << line.strip.downcase
     end
+  end
+
+  def map_digits_to_letters
+    @numbers.to_s.chars.map { |number| LETTERS[number.to_i] }
+  end
+
+  def mix_up_letters
+    ((@min_count-1)..(@total_length-1)).each do |i|
+      mix_up_1 = @mapped_letters[0..i]
+      mix_up_2 = keys[(i+1)..(@total_length-1)]
+      next if mix_up_1.length < @min_count || mix_up_2.length < @min_count
+    end
+  end
+
+  def form_words
+    
   end
 
 end
