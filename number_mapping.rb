28d6c033e30e44e053c4255ebf4ddff6cd88180d
Mothirajha <mothi@Mothirajhas-MacBook-Pro.local>
2019-01-10 02:28:37 +0530

    [Update] mixing up of letters and finding words from dictionary

--- number_mapping.rb
+++ number_mapping.rb
@@ -1,6 +1,7 @@
+require 'pry'
 class NumberMapping
 
-  attr_reader :dictionary, :numbers
+  attr_reader :dictionary, :numbers, :result
   attr_accessor :mapped_letters
 
   LETTERS = {
@@ -11,7 +12,7 @@ class NumberMapping
     6 => ["m", "n", "o"],
     7 => ["p", "q", "r", "s"],
     8 => ["t", "u", "v"],
-    9 => ["w", "x", "y", "z"],
+    9 => ["w", "x", "y", "z"]
   }
 
   def initialize(numbers)
@@ -21,6 +22,7 @@ class NumberMapping
     @mapped_letters = map_digits_to_letters
     @total_length = @mapped_letters.length
     @min_count = 3
+    @result = []
   end
 
   def load_dictionary
@@ -37,14 +39,21 @@ class NumberMapping
   def mix_up_letters
     ((@min_count-1)..(@total_length-1)).each do |i|
       mix_up_1 = @mapped_letters[0..i]
-      mix_up_2 = keys[(i+1)..(@total_length-1)]
+      mix_up_2 = @mapped_letters[(i+1)..(@total_length-1)]
       next if mix_up_1.length < @min_count || mix_up_2.length < @min_count
+      words_1 = form_words(mix_up_1)
+      words_2 = form_words(mix_up_2)
+      @result << [(words_1 & @dictionary), (words_2 & @dictionary)]
     end
   end
 
-  def form_words
-    
+  def form_words(array)
+    array.shift.product(*array).map(&:join)
   end
 
 end
+
+a = NumberMapping.new("2282668687");
+a.mix_up_letters
+a.result
 
